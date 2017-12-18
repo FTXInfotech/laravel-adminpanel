ffae1c31cb5ec0ebe739656f7a46a28ef87e4351
Viral Solani <viral.solani@gmail.com>
2017-12-18 23:06:23 +0530

    merge branch develop

---
+++ resources/views/backend/menus/index.blade.php
@@ -0,0 +1,100 @@
+@extends ('backend.layouts.app')
+
+@section ('title', trans('labels.backend.menus.management'))
+
+@section('after-styles')
+    {{ Html::style("css/backend/plugin/datatables/dataTables.bootstrap.min.css") }}
+@endsection
+
+@section('page-header')
+    <h1>{{ trans('labels.backend.menus.management') }}</h1>
+@endsection
+
+@section('content')
+    <div class="box box-info">
+        <div class="box-header with-border">
+            <h3 class="box-title">{{ trans('labels.backend.menus.management') }}</h3>
+
+            <div class="box-tools pull-right">
+                @include('backend.menus.partials.header-buttons')
+            </div>
+        </div><!-- /.box-header -->
+
+        <div class="box-body">
+            <div class="table-responsive data-table-wrapper">
+                <table id="menus-table" class="table table-condensed table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>{{ trans('labels.backend.menus.table.name') }}</th>
+                            <th>{{ trans('labels.backend.menus.table.type') }}</th>
+                            <th>{{ trans('labels.backend.menus.table.createdat') }}</th>
+                            <th>{{ trans('labels.general.actions') }}</th>
+                        </tr>
+                    </thead>
+                    <thead class="transparent-bg">
+                        <tr>
+                            <th>
+                                {!! Form::text('first_name', null, ["class" => "search-input-text form-control", "data-column" => 0, "placeholder" => trans('labels.backend.menus.table.name')]) !!}
+                                <a class="reset-data" href="javascript:void(0)"><i class="fa fa-times"></i></a>
+                            </th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
+            </div><!--table-responsive-->
+        </div><!-- /.box-body -->
+    </div><!--box-->
+
+    <!--<div class="box box-info">
+        <div class="box-header with-border">
+            <h3 class="box-title">{{ trans('history.backend.recent_history') }}</h3>
+            <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+            </div><!-- /.box tools -->
+        </div><!-- /.box-header -->
+        <div class="box-body">
+            {{-- {!! history()->renderType('Blog') !!} --}}
+        </div><!-- /.box-body -->
+    </div><!--box box-success-->
+@endsection
+
+@section('after-scripts')
+    {{-- For DataTables --}}
+    @include('includes.datatables')
+
+    <script>
+        $(function() {
+            var dataTable = $('#menus-table').dataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: '{{ route("admin.menus.get") }}',
+                    type: 'post'
+                },
+                columns: [
+                    {data: 'name', name: '{{config('access.menus_table')}}.name'},
+                    {data: 'type', name: '{{config('access.menus_table')}}.type'},
+                    {data: 'created_at', name: '{{config('access.menus_table')}}.created_at'},
+                    {data: 'actions', name: 'actions', searchable: false, sortable: false}
+                ],
+                order: [[3, "asc"]],
+                searchDelay: 500,
+                dom: 'lBfrtip',
+                buttons: {
+                    buttons: [
+                        { extend: 'copy', className: 'copyButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'csv', className: 'csvButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'excel', className: 'excelButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'pdf', className: 'pdfButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'print', className: 'printButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }}
+                    ]
+                }
+            });
+
+            Backend.DataTableSearch.init(dataTable);
+        });
+    </script>
+@endsection
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,6 +15,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertSee('Laravel AdminPanel');
     }
 }
