372d94bad7219823d375a27468731346e4bf0ee1
Viral Solani <viral.solani@gmail.com>
2018-01-02 16:51:50 +0530

    Unit test cases for Role

---
+++ tests/Unit/Models/RoleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Tests\TestCase;
+
+class RoleTest extends TestCase
+{
+
+    /** @test */
+    public function a_role_has_users()
+    {
+        $this->assertInstanceOf(
+            'Illuminate\Database\Eloquent\Collection', $this->adminRole->users
+        );
+    }
+
+    /** @test */
+    public function a_role_has_permissions()
+    {
+        $this->assertInstanceOf(
+            'Illuminate\Database\Eloquent\Collection', $this->adminRole->permissions
+        );
+    }
+}
