e1bb40669e73217306c251772bc6d38f228f1441
Viral Solani <viral.solani@gmail.com>
2018-01-02 11:22:52 +0000

    Apply fixes from StyleCI

--- tests/Feature/Backend/ManageRolesTest.php
+++ tests/Feature/Backend/ManageRolesTest.php
@@ -2,13 +2,8 @@
 
 namespace Tests\Feature\Backend;
 
-use App\Models\Access\Permission\Permission;
-use App\Models\Access\Role\Role;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class ManageRolesTest extends TestCase
 {
-
 }
