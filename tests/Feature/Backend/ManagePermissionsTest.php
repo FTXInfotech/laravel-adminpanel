2a07f8ac1d6789156795e9774f70422fecb93609
Viral Solani <viral.solani@gmail.com>
2018-01-02 23:30:48 +0530

    Feature Tests For Permission Module

---
+++ tests/Feature/Backend/ManagePermissionsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Backend;
+
+use App\Models\Access\Permission\Permission;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class ManagePermissionsTest extends TestCase
+{
+    /** @test */
+    public function a_user_can_view_permissions()
+    {
+        $this->actingAs($this->admin)
+            ->get(route('admin.access.permission.index'))
+            ->assertViewIs('backend.access.permissions.index')
+            ->assertSee(trans('labels.backend.access.permissions.management'))
+            ->assertSee('Export')
+            ->assertSee('Action');
+    }
+
+    /** @test */
+    public function a_permission_requires_a_name()
+    {
+        $permission = make(Permission::class, ['name' => null])->toArray();
+
+        return $this->withExceptionHandling()
+                    ->actingAs($this->admin)
+                    ->post(route('admin.access.permission.store'), $permission)
+                    ->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function a_permission_requires_a_display_name()
+    {
+        $permission = make(Permission::class, ['display_name' => null])->toArray();
+
+        return $this->withExceptionHandling()
+                    ->actingAs($this->admin)
+                    ->post(route('admin.access.permission.store'), $permission)
+                    ->assertSessionHasErrors('display_name');
+    }
+}
