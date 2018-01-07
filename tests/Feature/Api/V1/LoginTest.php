c9c623af76a07c3edae0f7b88924f51970214f8b
Viral Solani <viral.solani@gmail.com>
2018-01-07 02:45:45 +0000

    Apply fixes from StyleCI

--- tests/Feature/Api/V1/LoginTest.php
+++ tests/Feature/Api/V1/LoginTest.php
@@ -2,25 +2,22 @@
 
 namespace Tests\Feature\Api\V1;
 
-use Tests\TestCase;
-use App\Models\Access\User\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Event;
-use App\Events\Frontend\Auth\UserLoggedIn;
+use Tests\TestCase;
 
-class AuthTest extends TestCase
+class LoginTest extends TestCase
 {
     /** @test */
     public function users_can_login_through_api()
     {
         $res = $this->json('POST', '/api/v1/auth/login', [
-                'email' => $this->user->email,
-                'password' => '1234'
+                'email'    => $this->user->email,
+                'password' => '1234',
             ])
             ->assertStatus(200)
             ->assertJsonStructure([
                 'message',
-                'token'
+                'token',
             ]);
     }
 }
