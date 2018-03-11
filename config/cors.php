bcc401a4f48f1de84b55e1046bf32cfac29d9b79
Viral Solani <viral.solani@gmail.com>
2018-03-11 23:46:42 +0530

    add spatie/cors package

---
+++ config/cors.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+     * A cors profile determines which origins, methods, headers are allowed for
+     * a given requests. The `DefaultProfile` reads its configuration from this
+     * config file.
+     *
+     * You can easily create your own cors profile.
+     * More info: https://github.com/spatie/laravel-cors/#creating-your-own-cors-profile
+     */
+    'cors_profile' => Spatie\Cors\CorsProfile\DefaultProfile::class,
+
+    /*
+     * This configuration is used by `DefaultProfile`.
+     */
+    'default_profile' => [
+
+        'allow_origins' => [
+            '*',
+        ],
+
+        'allow_methods' => [
+            'POST',
+            'GET',
+            'OPTIONS',
+            'PUT',
+            'PATCH',
+            'DELETE',
+        ],
+
+        'allow_headers' => [
+            'Content-Type',
+            'X-Auth-Token',
+            'Origin',
+            'Authorization',
+        ],
+
+        'forbidden_response' => [
+            'message' => 'Forbidden (cors).',
+            'status' => 403,
+        ],
+
+        /*
+         * Preflight request will respond with value for the max age header.
+         */
+        'max_age' => 60 * 60 * 24,
+    ],
+];
--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-
 /**
  * Laravel - A PHP Framework For Web Artisans.
  *
