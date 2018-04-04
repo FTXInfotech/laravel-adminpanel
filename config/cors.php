6922a29f0a3cfee1bc3ef8ce8e3b7c46b974ce27
Viral Solani <viral.solani@gmail.com>
2018-04-04 14:55:07 +0530

    resolve cors error

--- config/cors.php
+++ config/cors.php
@@ -39,6 +39,15 @@ return [
             'Authorization',
         ],
 
+         'expose_headers' => [
+            'Cache-Control',
+            'Content-Language',
+            'Content-Type',
+            'Expires',
+            'Last-Modified',
+            'Pragma',
+        ],
+
         'forbidden_response' => [
             'message' => 'Forbidden (cors).',
             'status'  => 403,
