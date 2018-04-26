f03d52110d0dfc0f9cdf4ad84f23a9af25e91719
Vipul Basapati <basapativipulkumar@gmail.com>
2018-04-26 23:41:39 +0530

    Added new env parameters for from email in .env.example as MAIL_USERNAME is not a proper email and SwiftMailer uses it send email and if it is not a proper email, it will throw error

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_USERNAME', 'viral.solani@gmail.com'),
+        'address' => env('MAIL_FROM', 'viral.solani@gmail.com'),
         'name'    => env('MAIL_FROM_NAME', 'Admin'),
     ],
 
