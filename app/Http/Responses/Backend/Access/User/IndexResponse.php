0dd3180c9283a5456665bb90b791ee24e041f38c
Viral Solani <viral.solani@gmail.com>
2018-05-07 08:35:31 +0000

    Apply fixes from StyleCI

--- app/Http/Responses/Backend/Access/User/IndexResponse.php
+++ app/Http/Responses/Backend/Access/User/IndexResponse.php
@@ -7,10 +7,10 @@ use Illuminate\Contracts\Support\Responsable;
 class IndexResponse implements Responsable
 {
     /**
-     * In Response
+     * In Response.
      *
      * @param \App\Http\Requests\Request $request
-     * 
+     *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function toResponse($request)
