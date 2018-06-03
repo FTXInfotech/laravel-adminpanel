addee78fbf17a1d1c3ce0905a56dd0ed6a6df70a
Vipul Basapati <basapativipulkumar@gmail.com>
2018-06-03 20:26:46 +0530

    Page Module Responsable Trait implementation complete

--- app/Http/Controllers/Backend/Pages/PagesController.php
+++ app/Http/Controllers/Backend/Pages/PagesController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Backend\Pages;
 
+use App\Models\Page\Page;
 use App\Http\Controllers\Controller;
+use App\Http\Responses\ViewResponse;
+use App\Http\Responses\RedirectResponse;
+use App\Http\Responses\Backend\Page\EditResponse;
+use App\Repositories\Backend\Pages\PagesRepository;
+use App\Http\Requests\Backend\Pages\EditPageRequest;
+use App\Http\Requests\Backend\Pages\StorePageRequest;
 use App\Http\Requests\Backend\Pages\CreatePageRequest;
 use App\Http\Requests\Backend\Pages\DeletePageRequest;
-use App\Http\Requests\Backend\Pages\EditPageRequest;
 use App\Http\Requests\Backend\Pages\ManagePageRequest;
-use App\Http\Requests\Backend\Pages\StorePageRequest;
 use App\Http\Requests\Backend\Pages\UpdatePageRequest;
-use App\Models\Page\Page;
-use App\Repositories\Backend\Pages\PagesRepository;
 
 /**
  * Class PagesController.
@@ -30,70 +33,69 @@ class PagesController extends Controller
     /**
      * @param \App\Http\Requests\Backend\Pages\ManagePageRequest $request
      *
-     * @return mixed
+     * @return \App\Http\Responses\ViewResponse
      */
     public function index(ManagePageRequest $request)
     {
-        return view('backend.pages.index');
+        return new ViewResponse('backend.pages.index');        
     }
 
     /**
      * @param \App\Http\Requests\Backend\Pages\CreatePageRequest $request
      *
-     * @return mixed
+     * @return \App\Http\Responses\ViewResponse
      */
     public function create(CreatePageRequest $request)
     {
-        return view('backend.pages.create');
+        return new ViewResponse('backend.pages.create');
     }
 
     /**
      * @param \App\Http\Requests\Backend\Pages\StorePageRequest $request
      *
-     * @return mixed
+     * @return \App\Http\Responses\RedirectResponse
      */
     public function store(StorePageRequest $request)
     {
         $this->pages->create($request->except(['_token']));
 
-        return redirect()->route('admin.pages.index')->withFlashSuccess(trans('alerts.backend.pages.created'));
+        return new RedirectResponse('admin.pages.index', ['flash_success' => trans('alerts.backend.pages.created')]);
     }
 
     /**
      * @param \App\Models\Page\Page                            $page
      * @param \App\Http\Requests\Backend\Pages\EditPageRequest $request
      *
-     * @return mixed
+     * @return \App\Http\Responses\Backend\Page\EditResponse
      */
     public function edit(Page $page, EditPageRequest $request)
     {
-        return view('backend.pages.edit')
-            ->withPage($page);
+        return new EditResponse($page);
     }
 
     /**
      * @param \App\Models\Page\Page                              $page
      * @param \App\Http\Requests\Backend\Pages\UpdatePageRequest $request
      *
-     * @return mixed
+     * @return \App\Http\Responses\RedirectResponse
      */
     public function update(Page $page, UpdatePageRequest $request)
     {
         $this->pages->update($page, $request->except(['_method', '_token']));
 
-        return redirect()->route('admin.pages.index')->withFlashSuccess(trans('alerts.backend.pages.updated'));
+        return new RedirectResponse('admin.pages.index', ['flash_success' => trans('alerts.backend.pages.updated')]);
     }
 
     /**
      * @param \App\Models\Page\Page                              $page
      * @param \App\Http\Requests\Backend\Pages\DeletePageRequest $request
      *
-     * @return mixed
+     * @return \App\Http\Responses\RedirectResponse
      */
     public function destroy(Page $page, DeletePageRequest $request)
     {
         $this->pages->delete($page);
 
-        return redirect()->route('admin.pages.index')->withFlashSuccess(trans('alerts.backend.pages.deleted'));
+        return new RedirectResponse('admin.pages.index', ['flash_success' => trans('alerts.backend.pages.deleted')]);
     }
 }
---
+++ app/Http/Responses/Backend/Page/CreateResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Responses\Backend\Page;
+
+use Illuminate\Contracts\Support\Responsable;
+
+class CreateResponse implements Responsable
+{
+    /**
+     * In Response.
+     *
+     * @param \App\Http\Requests\Request $request
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function toResponse($request)
+    {
+        //
+    }
+}
