52e7b506adc6090fd66821064c92d26d54e370e4
Vipul Basapati <basapativipulkumar@gmail.com>
2018-06-23 22:53:44 +0530

    Solved BlogCategory and BlogTag breadcrumbs issue, they were not showing up

--- app/Http/Breadcrumbs/Backend/Blog_Category.php
+++ app/Http/Breadcrumbs/Backend/Blog_Category.php
@@ -1,16 +1,16 @@
 <?php
 
-Breadcrumbs::register('admin.blogcategories.index', function ($breadcrumbs) {
+Breadcrumbs::register('admin.blogCategories.index', function ($breadcrumbs) {
     $breadcrumbs->parent('admin.dashboard');
-    $breadcrumbs->push(trans('menus.backend.blogcategories.management'), route('admin.blogcategories.index'));
+    $breadcrumbs->push(trans('menus.backend.blogcategories.management'), route('admin.blogCategories.index'));
 });
 
-Breadcrumbs::register('admin.blogcategories.create', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin.blogcategories.index');
-    $breadcrumbs->push(trans('menus.backend.blogcategories.create'), route('admin.blogcategories.create'));
+Breadcrumbs::register('admin.blogCategories.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('admin.blogCategories.index');
+    $breadcrumbs->push(trans('menus.backend.blogcategories.create'), route('admin.blogCategories.create'));
 });
 
-Breadcrumbs::register('admin.blogcategories.edit', function ($breadcrumbs, $id) {
-    $breadcrumbs->parent('admin.blogcategories.index');
-    $breadcrumbs->push(trans('menus.backend.blogcategories.edit'), route('admin.blogcategories.edit', $id));
+Breadcrumbs::register('admin.blogCategories.edit', function ($breadcrumbs, $id) {
+    $breadcrumbs->parent('admin.blogCategories.index');
+    $breadcrumbs->push(trans('menus.backend.blogcategories.edit'), route('admin.blogCategories.edit', $id));
 });
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -36,9 +36,7 @@
 
         <!-- Scripts -->
         <script>
-            window.Laravel = <?php echo json_encode([
-                'csrfToken' => csrf_token(),
-            ]); ?>
+            window.Laravel = {!! json_encode([ 'csrfToken' => csrf_token() ]) !!};
         </script>
     </head>
     <body class="skin-{{ config('backend.theme') }} {{ config('backend.layout') }}">
