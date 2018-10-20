70d484ac16f24fddb95b10911eb0eaf710e1901d
Vipul Basapati <basapativipulkumar@gmail.com>
2018-10-21 01:18:04 +0530

    Fixed StyleCI issues

--- app/Http/Controllers/Frontend/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Frontend/Auth/ResetPasswordController.php
@@ -96,7 +96,7 @@ class ResetPasswordController extends Controller
      * Get the response for a successful password reset.
      *
      * @param \Illuminate\Http\Request $request
-     * @param string $response
+     * @param string                   $response
      *
      * @return \Illuminate\Http\RedirectResponse
      */
