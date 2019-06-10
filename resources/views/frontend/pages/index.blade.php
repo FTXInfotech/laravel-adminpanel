509d2b2575179d7d10724bc61536b663d6b1af16
Vidhyut Pandya <vidhyutpandya99@gmail.com>
2019-06-10 19:49:03 +0530

    Issue : #372 - Fixed meta tags not populating for pages

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -1,5 +1,9 @@
 @extends('frontend.layouts.app')
 
+@section('title'){{ $page->seo_title }}@endsection
+@section('meta_description'){{ $page->seo_description }}@endsection
+@section('meta_keywords'){{ $page->seo_keyword }}@endsection
+
 @section('content')
     {!! $page->description !!}                    
 @endsection
