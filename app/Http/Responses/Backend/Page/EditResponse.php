b44e1f82c7584d36c9288c410ccf41e23bc6eb68
Viral Solani <viralsolani@users.noreply.github.com>
2021-10-05 21:44:04 +0000

    Apply fixes from StyleCI

--- app/Http/Responses/Backend/Page/EditResponse.php
+++ app/Http/Responses/Backend/Page/EditResponse.php
@@ -12,7 +12,7 @@ class EditResponse implements Responsable
     protected $page;
 
     /**
-     * @param \App\Models\Page\Page $page
+     * @param  \App\Models\Page\Page  $page
      */
     public function __construct($page)
     {
@@ -22,8 +22,7 @@ class EditResponse implements Responsable
     /**
      * toReponse.
      *
-     * @param \Illuminate\Http\Request $request
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function toResponse($request)
--- app/Notifications/Frontend/Auth/UserNeedsConfirmation.php
+++ app/Notifications/Frontend/Auth/UserNeedsConfirmation.php
@@ -31,8 +31,7 @@ class UserNeedsConfirmation extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param mixed $notifiable
-     *
+     * @param  mixed  $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -43,8 +42,7 @@ class UserNeedsConfirmation extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param mixed $notifiable
-     *
+     * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
